8a930597f88b95b8994dc00dc2a17905fd658de4
Sean Ahern <seanpahern@gmail.com>
2022-06-02 17:32:04 -0700

    all

--- index.js
+++ index.js
@@ -12,41 +12,53 @@ const BODY_JIRA_REGEX = /[A-Z0-9]+-\d+/g;
 console.log("Hello");
 
 const run = async() => {
-  const token = core.getInput('token', {required: true});
-  const octokit = getOctokit(token);
-
-  // Test
-  const repo = context.payload.repository.name;
-  const owner = context.payload.repository.full_name.split('/')[0];
-  const pullNumber = context.payload.pull_request.number;
-
-
-  const title = context.payload.pull_request.title;
-  const body = context.payload.pull_request.body;
-  let titleContainsJiraId = (title).match(JIRA_REGEX) !== null;
-  let bodyContainsJiraId = (body).match(BODY_JIRA_REGEX) !== null;
-  console.log("Title is: ", title)
-  console.log("Title Contain Jira ID? ", titleContainsJiraId);
-  console.log("Body Contain Jira ID? ", bodyContainsJiraId);
-
-  var message = ""
-  
-  var issue_number = pullNumber;
-  var pull_number = pullNumber;
-
-  octokit.rest.pulls.update({
-    owner,
-    repo,
-    pull_number,
-    title: "FY22-1290 " + title
-  });
   try {
-    await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-      owner,
-      repo,
-      issue_number,
-      body: "Hello"
-    });
+    const token = core.getInput('token', {required: true});
+    const octokit = getOctokit(token);
+
+    // Test
+    const repo = context.payload.repository.name;
+    const owner = context.payload.repository.full_name.split('/')[0];
+    const pullNumber = context.payload.pull_request.number;
+
+
+    const title = context.payload.pull_request.title;
+    const body = context.payload.pull_request.body;
+    let titleContainsJiraId = (title).match(JIRA_REGEX) !== null;
+    let bodyContainsJiraId = (body).match(BODY_JIRA_REGEX) !== null;
+    console.log("Title is: ", title)
+    console.log("Title Contain Jira ID? ", titleContainsJiraId);
+    console.log("Body Contain Jira ID? ", bodyContainsJiraId);
+
+    var message = ""
+    
+    var issue_number = pullNumber;
+    var pull_number = pullNumber;
+
+    if(titleContainsJiraId) {
+      console.log("Good to go! No action");
+      return false;
+    }
+
+    if(!titleContainsJiraId && !bodyContainsJiraId) {
+      // message = "No JIRA ID found in PR title or body. Please add";
+    }
+
+    if(!titleContainsJiraId && bodyContainsJiraId) {
+      var jiraID = bodyContainsJiraId[0];
+      await octokit.rest.pulls.update({
+        owner,
+        repo,
+        pull_number,
+        title: jiraID + " " + title
+      });
+      await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+        owner,
+        repo,
+        issue_number,
+        body: "Updating PR with Jira ID from PR description"
+      });
+    }
   } catch(e) {
 
     core.setFailed(e.message);
