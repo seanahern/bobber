ca07aa7a4398174a5405baf04b3a85720efca27c
Sean Ahern <seanpahern@gmail.com>
2022-06-02 17:23:32 -0700

    body title

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const {
   getOctokit
 } = require('@actions/github');
 
+const JIRA_REGEX = /[A-Z0-9]+-\d+/;
+const BODY_JIRA_REGEX = /[A-Z0-9]+-\d+/g;
+
 console.log("Hello");
 
 const run = async() => {
@@ -16,13 +19,18 @@ const run = async() => {
   const repo = context.payload.repository.name;
   const owner = context.payload.repository.full_name.split('/')[0];
   const pullNumber = context.payload.pull_request.number;
-  console.log([repo, owner])
 
-  console.log("title");
+
   const title = context.payload.pull_request.title;
-  console.log(title);
-  console.log("Contain Jira ID?", (title).match(/[A-Z0-9]+-\d+/));
+  const body = context.payload.pull_request.body;
+  let titleContainsJiraId = (title).match(JIRA_REGEX) !== null;
+  let bodyContainsJiraId = (body).match(BODY_JIRA_REGEX) !== null;
+  console.log("Title is: ", title)
+  console.log("Title Contain Jira ID? ", titleContainsJiraId);
+  console.log("Body Contain Jira ID? ", bodyContainsJiraId);
 
+  var message = ""
+  
   var issue_number = pullNumber;
   try {
     await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
